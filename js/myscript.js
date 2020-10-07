7404fc851e6145040064bae57b6babad0ea19fd4
desmond93 <bondarenkomaxim93@gmail.com>
2020-10-07 20:09:50 +0300

    added landingPage3

---
+++ js/myscript.js
@@ -0,0 +1,114 @@
+$('.fourth_block_1 .arrow_cross').on('click', function() {
+  if ($('.fourth_block_1 .arrow_cross').text() == 'know more →') { 
+  otkat(); 
+  $('.fourth_block_1 p').html('Our most popular service is our Virtual Receptionist. We know that sometimes it is something and you middle most this job was posted less then five minutes ago, and er think it is a good mathch for you. If you submit a proposal now, you will be one of the first candidates. You may even catch the client while they are still online');
+  $('.fourth_block_1 .arrow_cross').html('✖');  
+  $('.fourth_block_1_1').css('background', '#afeeea');
+  $('.fourth_block_1_1 h3').css('color', '#132928');
+  $('.fourth_block_1_1 p').css('color', '#132928');
+  $('.fourth_block_1_1 .arrow_cross').css('color', '#132928');
+  $('.fourth_block_1_1').css('padding', '60px 150px 50px 60px');
+  }
+  else {
+  otkat();
+}  
+});
+
+$('.fourth_block_2 .arrow_cross').on('click', function() {
+  if ($('.fourth_block_2 .arrow_cross').text() == 'know more →') { 
+  otkat(); 
+  $('.fourth_block_2 p').html('Our most popular service is our Virtual Receptionist. We know that sometimes it is something and you middle most this job was posted less then five minutes ago, and er think it is a good mathch for you. If you submit a proposal now, you will be one of the first candidates. You may even catch the client while they are still online');
+  $('.fourth_block_2 .arrow_cross').html('✖');  
+  $('.fourth_block_2_1').css('background', '#afeeea');
+  $('.fourth_block_2_1 h3').css('color', '#132928');
+  $('.fourth_block_2_1 p').css('color', '#132928');
+  $('.fourth_block_2_1 .arrow_cross').css('color', '#132928');
+  $('.fourth_block_2_1').css('padding', '20px 20px 20px 60px');
+  }
+  else {
+  otkat();
+}  
+});
+
+$('.fourth_block_3 .arrow_cross').on('click', function() {
+  if ($('.fourth_block_3 .arrow_cross').text() == 'know more →') { 
+  otkat(); 
+  $('.fourth_block_3 p').html('Our most popular service is our Virtual Receptionist. We know that sometimes it is something and you middle most this job was posted less then five minutes ago, and er think it is a good mathch for you. If you submit a proposal now, you will be one of the first candidates. You may even catch the client while they are still online');
+  $('.fourth_block_3 .arrow_cross').html('✖');  
+  $('.fourth_block_3_1').css('background', '#afeeea');
+  $('.fourth_block_3_1 h3').css('color', '#132928');
+  $('.fourth_block_3_1 p').css('color', '#132928');
+  $('.fourth_block_3_1 .arrow_cross').css('color', '#132928');
+  $('.fourth_block_3_1').css('padding', '80px 150px 50px 60px');
+  }
+  else {
+  otkat();
+}  
+});
+
+$('.fourth_block_4 .arrow_cross').on('click', function() {
+  if ($('.fourth_block_4 .arrow_cross').text() == 'know more →') { 
+  otkat(); 
+  $('.fourth_block_4 p').html('Our most popular service is our Virtual Receptionist. We know that sometimes it is something and you middle most this job was posted less then five minutes ago, and er think it is a good mathch for you. If you submit a proposal now, you will be one of the first candidates. You may even catch the client while they are still online');
+  $('.fourth_block_4 .arrow_cross').html('✖');  
+  $('.fourth_block_4_1').css('background', '#afeeea');
+  $('.fourth_block_4_1 h3').css('color', '#132928');
+  $('.fourth_block_4_1 p').css('color', '#132928');
+  $('.fourth_block_4_1 .arrow_cross').css('color', '#132928');
+  $('.fourth_block_4_1').css('padding', '25px 20px 20px 60px');
+  }
+  else {
+  otkat();
+}  
+});
+
+$('.fourth_block_5 .arrow_cross').on('click', function() {
+  if ($('.fourth_block_5 .arrow_cross').text() == 'know more →') { 
+  otkat(); 
+  $('.fourth_block_5 p').html('Our most popular service is our Virtual Receptionist. We know that sometimes it is something and you middle most this job was posted less then five minutes ago, and er think it is a good mathch for you. If you submit a proposal now, you will be one of the first candidates. You may even catch the client while they are still online');
+  $('.fourth_block_5 .arrow_cross').html('✖');  
+  $('.fourth_block_5_1').css('background', '#afeeea');
+  $('.fourth_block_5_1 h3').css('color', '#132928');
+  $('.fourth_block_5_1 p').css('color', '#132928');
+  $('.fourth_block_5_1 .arrow_cross').css('color', '#132928');
+  $('.fourth_block_5_1').css('padding', '25px 20px 20px 60px');
+  }
+  else {
+  otkat();
+}  
+});
+
+$('.fourth_block_6 .arrow_cross').on('click', function() {
+  if ($('.fourth_block_6 .arrow_cross').text() == 'know more →') { 
+  otkat(); 
+  $('.fourth_block_6 p').html('Our most popular service is our Virtual Receptionist. We know that sometimes it is something and you middle most this job was posted less then five minutes ago, and er think it is a good mathch for you. If you submit a proposal now, you will be one of the first candidates. You may even catch the client while they are still online');
+  $('.fourth_block_6 .arrow_cross').html('✖');  
+  $('.fourth_block_6_1').css('background', '#afeeea');
+  $('.fourth_block_6_1 h3').css('color', '#132928');
+  $('.fourth_block_6_1 p').css('color', '#132928');
+  $('.fourth_block_6_1 .arrow_cross').css('color', '#132928');
+  $('.fourth_block_6_1').css('padding', '50px 150px 50px 60px');
+  }
+  else {
+  otkat();
+}  
+});
+
+function otkat () {
+  $('.fourth_block p').html('Our most popular service is our Virtual Receptionist. We know that sometimes it is something and you middle..');
+  $('.arrow_cross').html('know more →');
+  $('.fourth_block').css('background', 'rgb(0, 0, 0, 0.7)');
+  $('.fourth_block h3').css('color', 'rgba(249, 251, 251, 0.9)');
+  $('.fourth_block p').css('color', 'rgba(249, 251, 251, 0.9)');
+  $('.fourth_block .arrow_cross').css('color', 'rgba(249, 251, 251, 0.9)');
+  $('.fourth_block_1_1').css('padding', '150px 100px 50px 60px');
+  $('.fourth_block_2_1').css('padding', '150px 90px 50px 60px');
+  $('.fourth_block_3_1').css('padding', '150px 180px 50px 60px');
+  $('.fourth_block_4_1').css('padding', '150px 90px 50px 60px');
+  $('.fourth_block_5_1').css('padding', '120px 120px 50px 60px');
+  $('.fourth_block_6_1').css('padding', '120px 180px 50px 60px');
+}
+
+
+
+  
